24ff580b0eef50ec8ea3bf3d6e581db37f588c50
BISTOUFLEX <romain.moussie@gmail.com>
2023-02-06 15:13:17 +0100

    Update TP2.R

--- TP2.R
+++ TP2.R
@@ -10,7 +10,7 @@ cat("le nombres de lignes de la Dataframe Pokemon est", l, "\n")
 c <- ncol(pokemon) 
 cat("le nombres de colonnes de la Dataframe Pokemon est", c, "\n")
 
-#Affichez le nom des colonne
+#Affichez le nom des colonnes
 n <- colnames(pokemon)
 cat("le nom des colonnes de la Dataframe Pokemon est", n, "\n")
 
@@ -91,4 +91,55 @@ type_table_sorted
 
 ##EXERCICE 3 :
 
+#Sélectionne la colonne nom et is_legendary :
+requete_1 <- pokemon[c("nom", "is_legendary")]
+dim(requete_1)
+print(requete_1)
 
+#Sélectionne les 50 premières lignes et les deux premières colonnes :
+requete_2 <- pokemon[1:50, c(1,2)]
+dim(requete_2)
+print(requete_2,n=50)
+
+#Sélectionne les 50 premières lignes et les deux premières colonnes :
+requete_3 <- pokemon[1:10]
+dim(requete_3)
+requete_3
+#Sélectionnez toutes les colonnes sauf la dernière :
+requete_4 <- pokemon[1:ncol(pokemon)-1]
+dim(requete_4)
+print(requete_4,n=nrow(requete_4))
+
+
+#Triez le dataset par ordre alphabétique et afficher le nom du pokemon de la première ligne :
+requete_5 <- data.frame(sort(pokemon$nom, decreasing = F))
+requete_5[1,]
+dim(requete_5)
+
+#Triez le dataset par weight_kg en ordre décroissant, et afficher le nom du pokemon de la première ligne :
+requete_6 <- pokemon[order(-pokemon$weight_kg),]
+requete_6[1,]$nom
+
+#Triez le dataset par attack en ordre décroissant puis par speed en ordre croissant, et afficher le nom des pokemons des 10 premières lignes :
+requete_7 <- pokemon[order(-pokemon$attack,pokemon$speed),]
+requete_7[1:10,]$nom
+dim(requete_7)
+
+##Exercice 4 : Tris et Filtres
+
+#a.Filtrez sur les pokemons qui ont 150 ou plus d’attack puis trier le résultat par ordre décroissant d’attack:
+pokemon_filtered <- pokemon[pokemon$attack >= 150,c("nom","attack")]
+Exo_2_requete_1 <- pokemon_filtered[order(-pokemon_filtered$attack),]
+dim(Exo_2_requete_1)
+Exo_2_requete_1
+
+#b. Filtrez sur les pokemons de type dragon,ghost,psychic et dark:
+Exo_2_requete_2 <- pokemon[pokemon$type %in% c("dragon", "ghost", "psychic", "dark"),c("nom","type")]
+dim(Exo_2_requete_2)
+print(Exo_2_requete_2,n=nrow(Exo_2_requete_2))
+
+#c. Filtrez sur les pokemons de type fire avec plus de 100 d’attack, puis trier le résultat par ordre décroissant d’attack :
+pokemon_fire_attack_100 <- pokemon[pokemon$type == "fire" & pokemon$attack > 100,c("nom","type","attack")]
+Exo_2_requete_3 <- pokemon_fire_attack_100[order(-pokemon_fire_attack_100$attack), ]
+dim(Exo_2_requete_3)
+Exo_2_requete_3
